2c05b0b6d980a9b85450a73577b75c672f543bb0
rodo <brodrigue@protonmail.co>
2023-08-13 21:23:14 +0000

    feature(api): search endpoint

--- src/novels/novels.controller.ts
+++ src/novels/novels.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
 import { NovelsService } from "./novels.service";
 import { Request } from "src/types";
+import { SearchDto } from "./novels.dto";
 
 function getAge(birthdateString: string) {
   const today = new Date();
@@ -43,6 +44,28 @@ export class NovelsController {
     const isMature = age >= 18;
 
     const novels = await this.novelsService.getPublicAuthorNovels(authorId);
+
+    if (isMature) {
+      return novels;
+    }
+
+    return novels.filter((novel) => !novel.isMature);
+  }
+
+  @Post("/search")
+  async searchNovel(
+    @Req() req: Request,
+    @Body() dto: SearchDto,
+  ) {
+    const { searchTerms } = dto;
+    const birthdate = req.jwt.user.birthdate;
+    const birthdateString = new Date(birthdate).toString();
+    const age = getAge(birthdateString);
+    const isMature = age >= 18;
+
+    console.log(searchTerms);
+
+    const novels = await this.novelsService.searchNovel(searchTerms);
 
     if (isMature) {
       return novels;
